3c0e68beb2f258f4c91422011e7b677901d91290
themylogin <themylogin@gmail.com>
2025-03-20 12:59:04 +0100

    Fix build
    (cherry picked from commit ab19ab47ce3a49d121cca80869afe42e679063db)

--- generate.sh
+++ generate.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+docker pull debian:bookworm
 docker run --rm -v $(pwd):/work -w /work debian:bookworm sh -c 'apt-get update && apt-get install -y curl git libffi-dev python3-virtualenv && curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && PATH="$PATH:$HOME/.cargo/bin" python3 generate.py'
