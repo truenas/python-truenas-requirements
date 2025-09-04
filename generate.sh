8bf5e567df6531da3ab504b9b35a6dae94c2dd40
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-09-05 03:00:22 +0500

    Make sure generate.sh works nicely on arm

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker pull debian:trixie
-docker run --rm -v $(pwd):/work -w /work debian:trixie sh -c 'apt-get update && apt-get install -y curl git libffi-dev python3-virtualenv python3.13 python3.13-dev && curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && PATH="$PATH:$HOME/.cargo/bin" python3.13 generate.py'
+docker pull --platform linux/amd64 debian:trixie
+docker run --platform linux/amd64 --rm -v $(pwd):/work -w /work debian:trixie sh -c 'apt-get update && apt-get install -y curl git libffi-dev python3-virtualenv python3.13 python3.13-dev && curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && PATH="$PATH:$HOME/.cargo/bin" python3.13 generate.py'
